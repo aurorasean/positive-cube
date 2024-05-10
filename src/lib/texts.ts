045f838f0437017d7abd97f1c3ee6cf0c4c5fabd
Sean Thomas <aurorasean@gmail.com>
2024-05-10 18:18:27 +0100

    first

---
+++ src/lib/texts.ts
@@ -0,0 +1,28 @@
+export const texts = [
+    // { text: "I am a positive cube message, and you will be happy", color: getRandomColor() },
+    { text: "When it rains look for rainbows", color: getRandomColor() },
+    { text: "All great ideas are dangerous", color: getRandomColor() },
+    { text: "We are shaped by our thoughts, we become what we think", color: getRandomColor() },
+    { text: "Be less curious about people and more curious about ideas.", color: getRandomColor() },
+    { text: "What we know is a drop. What we don’t know is an ocean", color: getRandomColor() },
+    { text: "There’s no need to be perfect to inspire others", color: getRandomColor() },
+    { text: "Success is not something to wait for, it is something to work for", color: getRandomColor() },
+    { text: "Be someone you want to be around", color: getRandomColor() },
+    { text: "If you don’t know where you are going any road can take you there", color: getRandomColor() },
+    { text: "It’s never too late in the day to start your day over", color: getRandomColor() },
+    { text: "The noblest pleasure is the joy of understanding", color: getRandomColor() },
+    { text: "The future begins now", color: getRandomColor() },
+    { text: "Sometimes the people with the worst past, create the best future.", color: getRandomColor() },
+    { text: "Experience is the teacher of all things", color: getRandomColor() },
+    { text: "Believe half of what you see and nothing of what you hear", color: getRandomColor() },
+    { text: "Life is like a cup of tea. The sugar is all at the bottom", color: getRandomColor() },
+    { text: "To be old and wise, you must first be young and stupid", color: getRandomColor() },
+    { text: "True happiness is to enjoy the present without anxious dependence upon the future", color: getRandomColor() },
+    { text: "Everything you say should be true, but not everything true should be said", color: getRandomColor() },
+    { text: "A clean house is a sign of a misspent life.", color: getRandomColor() },
+    { text: "Be yourself; everyone else is already taken", color: getRandomColor() }
+];
+
+function getRandomColor(): number {
+    return Math.floor(Math.random() * 16777215);
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,349 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { tweened } from "svelte/motion";
+	import { texts } from "$lib/texts";
+
+	import * as SC from "svelte-cubed";
+	import { onMount } from "svelte";
+
+	import * as THREE from "three";
+	import { GLTFLoader, type GLTF } from "three/addons/loaders/GLTFLoader.js";
+	import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
+	import { Font, FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+	import { linear, quartInOut } from "svelte/easing";
+	let defaultStartHover = -0.5;
+	let defaultRotationTimer = 500;
+	let defaultEndHover = 0.01;
+	let hoverPosition = tweened(defaultStartHover, {
+		duration: 100,
+		easing: linear,
+	});
+	let rotationPosition = tweened(THREE.MathUtils.degToRad(15), {
+		duration: defaultRotationTimer,
+		easing: linear,
+	});
+
+	let isPlaying = false;
+	let model: GLTF;
+	let modelHover: THREE.Mesh;
+	let label: THREE.Mesh;
+	let hourGlass: THREE.Mesh;
+
+	let font: Font;
+	let camera: any;
+	let labelText = "The\nPositive\nCube";
+	let canvas: SC.Canvas;
+	let isHovered = false;
+	$: mainCubePosition = [0, 0, 0] as [number, number, number];
+	$: cubePos = [0, 0, $hoverPosition] as [number, number, number];
+	$: cubeRotation = [0, $rotationPosition, 0] as [number, number, number];
+	$: threeCamera = camera?.$$.ctx[camera?.$$.ctx.length - 1];
+	$: hourGlassPos = [0, 0.0, 0.6] as [number, number, number];
+	let defaultLabelposition = .95
+	$: labelPosition = [0, 0, defaultLabelposition] as [number, number, number];
+	let hourGlassRoationTween = tweened(THREE.MathUtils.degToRad(0), {
+		duration: 1000,
+		easing: quartInOut,
+	});
+	$: hourGlassRotation = [0, 0, $hourGlassRoationTween] as [
+		number,
+		number,
+		number,
+	];
+
+	function loadGLTF() {
+		const loader = new GLTFLoader();
+		return loader.loadAsync("Cube-Test.glb");
+	}
+	function loadGLTFHover() {
+		const loader = new GLTFLoader();
+		return loader.loadAsync("Cube-Test_hover.glb");
+	}
+	const loadLabel = (
+		textValue: string = labelText,
+		color: number = 0x00ff00,
+	) => {
+		const labelgeo = new TextGeometry(textValue, {
+			font: font,
+			size: 0.1,
+			depth: 0.001,
+		});
+		const labelmat = new THREE.MeshBasicMaterial({ color: color });
+		labelgeo.center();
+		labelgeo.computeBoundingSphere();
+
+		label = new THREE.Mesh(labelgeo, labelmat);
+	};
+
+	$: font, labelText, loadLabel();
+
+	const fontLoader = new FontLoader();
+	fontLoader.load(
+		"fonts/helvetiker_regular.typeface.json",
+		function (fontValue) {
+			font = fontValue;
+		},
+	);
+
+	const loadHourGlassMesh = () => {
+		const geometry = new THREE.PlaneGeometry(1, 1);
+		const texture = new THREE.TextureLoader().load("hour-glass-1.png");
+		texture.minFilter = THREE.NearestFilter;
+		texture.magFilter = THREE.NearestFilter;
+
+		const material = new THREE.MeshBasicMaterial({
+			side: THREE.DoubleSide,
+			transparent: true,
+			map: texture,
+		});
+		const plane = new THREE.Mesh(geometry, material);
+		hourGlass = plane;
+	};
+
+	onMount(() => {
+		// load the hover model
+		loadGLTFHover().then((_model) => {
+			var material2 = new THREE.MeshLambertMaterial({
+				color: 0x00ff22,
+				transparent: true,
+				opacity: 0.1,
+			});
+			_model.scene.traverse((child) => {
+				if (child instanceof THREE.Mesh) {
+					child.material = material2;
+					modelHover = child;
+				}
+			});
+			// modelHover = _model;
+		});
+		// load the main model
+		loadGLTF().then((_model) => (model = _model));
+		loadHourGlassMesh();
+	});
+
+	let isInCanvas = false;
+
+	const handleMouseEnter = (e: MouseEvent) => {
+		isInCanvas = true;
+	};
+	const handleMouseLeave = (e: MouseEvent) => {
+		isInCanvas = false;
+	};
+
+	const setToDefault = () => {
+		let elem = canvas.$$.ctx[1];
+		elem.style.cursor = "default";
+	};
+	const setToPoint = () => {
+		let elem = canvas.$$.ctx[1];
+		elem.style.cursor = "pointer";
+	};
+
+	const handleEvent = (clientX: number, clientY: number) => {
+		if (!isInCanvas || !threeCamera || !model || isPlaying) {
+			return false;
+		}
+
+		const raycaster = new THREE.Raycaster();
+		const pointerV2 = getMouseCordsAsV2(clientX, clientY);
+		raycaster.setFromCamera(pointerV2, threeCamera);
+		const intersects = raycaster.intersectObjects([model.scene]);
+		if (intersects.length > 0) {
+			hoverPosition.set(defaultEndHover);
+			// need to change the cursor to pointer
+			setToPoint();
+			isHovered = true;
+			return true;
+		} else {
+			hoverPosition.set(defaultStartHover);
+			setToDefault();
+			isHovered = false;
+			return false;
+		}
+	};
+
+	const getMouseCordsAsV2 = (clientX: number, clientY: number) => {
+		let elem = canvas.$$.ctx[1];
+		let x = (clientX / elem.clientWidth) * 2 - 1;
+		let y = -(clientY / elem.clientHeight) * 2 + 1;
+		return new THREE.Vector2(x, y);
+	};
+	const handleMouseMove = (e: MouseEvent) => {
+		handleEvent(e.clientX, e.clientY);
+	};
+
+	const splitTextAtCharCount = (text: string, charCount: number) => {
+		let splitText = text.split(" ");
+		let newText = "";
+		let count = 0;
+		for (let i = 0; i < splitText.length; i++) {
+			if (count + splitText[i].length > charCount) {
+				newText += "\n" + splitText[i] + " ";
+				count = splitText[i].length;
+			} else {
+				newText += splitText[i] + " ";
+				count += splitText[i].length;
+			}
+		}
+		return newText;
+	};
+
+	const setLabelBackToDefault = () => {
+		loadLabel();
+	};
+
+	const handleRandomSelector = () => {
+		// start the random selector
+		isPlaying = true;
+		hoverPosition.set(defaultStartHover);
+
+		// need to show thinking mode first
+		showThinking(() => {
+			animateTheTextOneCharAtATime(() => {
+				// after the text is done, show the hour glass
+				isPlaying = false;
+				setTimeout(() => {
+					setLabelBackToDefault();
+				}, 5000);
+			});
+		});
+
+		// after time x, show the normal text again
+
+		// get random text from texts
+	};
+	const showThinking = (nextFunction: Function) => {
+		labelPosition[2] = 0.6;
+		hourGlassPos[2] = defaultLabelposition;
+		hourGlassRoationTween.set(THREE.MathUtils.degToRad(90)).then(() => {
+			hourGlassRoationTween.set(THREE.MathUtils.degToRad(0)).then(() => {
+				hourGlassRoationTween
+					.set(THREE.MathUtils.degToRad(-90))
+					.then(() => {
+						hourGlassRoationTween
+							.set(THREE.MathUtils.degToRad(0))
+							.then(() => {
+								hourGlassPos[2] = 0.6;
+								labelPosition[2] = defaultLabelposition;
+								nextFunction();
+							});
+					});
+			});
+		});
+	};
+	const animateTheTextOneCharAtATime = (nextFunction: Function) => {
+		const randomText = texts[Math.floor(Math.random() * texts.length)];
+		const textValue = splitTextAtCharCount(randomText.text, 17);
+		const textEmpty = textValue.replace(/./g, " ");
+		for (let i = 0; i < textValue.length; i++) {
+			setTimeout(() => {
+				loadLabel(
+					textValue.substring(0, i) + textEmpty.substring(i),
+					randomText.color,
+				);
+				// if I am the last character then call the nextFunction
+				if (i === textValue.length - 1) {
+					nextFunction();
+				}
+			}, i * 75);
+		}
+	};
+
+	let touchStartTime = 0;
+	let isTouchedStill = false;
+	const handleTouchStart = (e: TouchEvent) => {
+		isInCanvas = true;
+		touchStartTime = Date.now();
+		isTouchedStill = true;
+		const inBlock = handleEvent(e.touches[0].clientX, e.touches[0].clientY);
+
+		// if the touch is longer than .5 seconds then it is a click
+		setTimeout(() => {
+			if (isTouchedStill && inBlock && !isPlaying) {
+				// start the random selector
+				handleRandomSelector();
+			}
+		}, 500); // 500ms (0.5 seconds
+	};
+	const handleTouchEnd = (e: TouchEvent) => {
+		isInCanvas = false;
+		isTouchedStill = false;
+	};
+	const handleTouchMove = (e: TouchEvent) => {
+		handleEvent(e.targetTouches[0].clientX, e.targetTouches[0].clientY);
+	};
+	const handleMouseClick = (e: MouseEvent) => {
+		if (isHovered && !isPlaying) {
+			// start the random selector
+			handleRandomSelector();
+		}
+	};
+
+	// do something
+</script>
+
+<div style="display: flex; flex-direction: column">
+	<div style="z-index: 1; display: none">
+		<input type="text" bind:value={labelText} />
+		<button on:click={() => hoverPosition.set(defaultEndHover)}
+			>click 1</button
+		>
+		<button on:click={() => hoverPosition.set(defaultStartHover)}
+			>click 0</button
+		>
+		<p>Scale: {$hoverPosition}</p>
+	</div>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<div
+		id="canvas"
+		on:mouseenter={handleMouseEnter}
+		on:mouseleave={handleMouseLeave}
+		on:mousemove={handleMouseMove}
+		on:mousedown={handleMouseClick}
+		on:touchstart={handleTouchStart}
+		on:touchend={handleTouchEnd}
+		on:touchmove={handleTouchMove}
+	>
+		​<SC.Canvas
+			bind:this={canvas}
+			antialias
+			background={new THREE.Color("white")}
+			stencil
+		>
+			{#if model}
+				<SC.Primitive
+					object={model.scene}
+					position={mainCubePosition}
+					rotation={cubeRotation}
+				>
+					{#if hourGlass}
+						<SC.Primitive
+							object={hourGlass}
+							position={hourGlassPos}
+							rotation={hourGlassRotation}
+						/>
+					{/if}
+					{#if modelHover}
+						<SC.Primitive object={modelHover} position={cubePos} />
+					{/if}
+
+					{#if label}
+						<SC.Primitive object={label} position={labelPosition} />
+					{/if}
+				</SC.Primitive>
+			{/if}
+			<SC.PerspectiveCamera bind:this={camera} position={[1, 1, 3]} />
+			<SC.OrbitControls
+				enablePan={false}
+				maxPolarAngle={1.6}
+				minPolarAngle={1}
+				maxDistance={5}
+				minDistance={5}
+				dampingFactor={0.1}
+				enableDamping={true}
+			/>
+			<SC.AmbientLight intensity={1} />
+			<SC.DirectionalLight intensity={2} position={[-2, 3, 2]} />
+			​</SC.Canvas
+		>
+	</div>
+</div>
